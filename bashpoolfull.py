cc02d8605802fa18734ebafa3a0dd18e32f953ea
danielegana <166064320+danielegana@users.noreply.github.com>
2024-05-16 09:28:26 -0400

    added bashpullfull

---
+++ bashpoolfull.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import numpy as np
+
+def testfun(x,y):
+    z=y
+    return z
+    
+def find_number(arr, target):
+    return target in arr
+
+def bashfun(run,tuple,selecttuples):
+        Rgit=tuple[0]
+        Rinit=tuple[1]
+        R0it=tuple[2]
+        slopeit=tuple[3]
+        inclinationit=tuple[4]
+        posangleit=tuple[5]
+        numpix=int(tuple[6])
+        bashcommand="./ssdisk"+" -run "+str(run)+" -Rmax "+str(150)+" -np "+str(numpix)
+        if find_number(selecttuples, 0):
+            bashcommand=bashcommand+" -Rg "+str(Rgit)
+        if find_number(selecttuples, 1):
+            bashcommand=bashcommand+" -Rin "+str(Rinit)
+        if find_number(selecttuples, 2):
+            bashcommand=bashcommand+" -R0 "+str(R0it)
+        if find_number(selecttuples, 3):
+            bashcommand=bashcommand+" -n "+str(slopeit)
+        if find_number(selecttuples, 4):
+            bashcommand=bashcommand+" -i "+str(np.arccos(inclinationit))
+        if find_number(selecttuples, 5):
+            bashcommand=bashcommand+" -phi "+str(np.arccos(posangleit))
+       # bashcommand="./ssdisk"+" -run "+str(run)+" -Rg "+str(Rgit)+" -Rin "+str(Rinit)+" -R0 "+str(R0it)+" -n "+str(slopeit)+" -i "+str(np.arccos(inclinationit))+" -phi "+str(np.arccos(posangleit))
+       # print(bashcommand)
+        os.system(bashcommand)
+        return run
